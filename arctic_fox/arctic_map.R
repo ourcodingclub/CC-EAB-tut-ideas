9a8b334f5b1746b493d488b3cd75520693e98dca
gndaskalova <gndaskalova@gmail.com>
2017-12-07 22:24:25 +0000

    Update arctic_map.R

--- arctic_fox/arctic_map.R
+++ arctic_fox/arctic_map.R
@@ -15,7 +15,12 @@ library(mapdata)
 library(rgbif)
 
 # Download data from GBIF ----
-arctic_fox <- occ_search(scientificName = "Vulpes lagopus", limit = 20000, hasCoordinate = TRUE, return = "data")
+
+# Here is the code so that you know how to do it, but please don't run it now as it takes a while
+# We have downloaded the data in advance and saved it as a .RData file
+# arctic_fox <- occ_search(scientificName = "Vulpes lagopus", limit = 20000, hasCoordinate = TRUE, return = "data")
+
+load("arctic_fox.Rdata")
 
 # Make a map ----
 # Get global spatial data
@@ -29,7 +34,7 @@ world <- map_data("world")
           axis.text = element_blank(), axis.ticks = element_blank(), 
           axis.title = element_blank()) + 
     coord_map("ortho", orientation = c(90, 0, 0), ylim = c(0, 90)) + 
-    geom_point(data = arctic_fox, aes(x = decimalLongitude, y = decimalLatitude, group = NULL), 
+    geom_point(data = arctic_fox, aes(x = decimallongitude, y = decimallatitude, group = NULL), 
                fill = "#5bbcd6", colour = "black", shape = 21, size = 4, alpha = 0.6))
 
 # Save map ----
@@ -44,7 +49,7 @@ ggsave(map, filename = "fox_map.pdf")
           axis.text = element_blank(), axis.ticks = element_blank(), 
           axis.title = element_blank()) + 
     coord_map("ortho", orientation = c(0, -60, 0)) + 
-    geom_point(data = arctic_fox, aes(x = decimalLongitude, y = decimalLatitude, group = NULL), 
+    geom_point(data = arctic_fox, aes(x = decimallongitude, y = decimallatitude, group = NULL), 
                fill = "#5bbcd6", colour = "black", shape = 21, size = 4, alpha = 0.6))
 
 # Interesting arctic fox sightings in South America and in the South Atlantic ocean!

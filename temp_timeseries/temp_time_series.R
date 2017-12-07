0ffde30c0e1e6d387bc54382847888cb293faebd
gndaskalova <gndaskalova@gmail.com>
2017-12-07 22:51:07 +0000

    Update temp_time_series.R

--- temp_timeseries/temp_time_series.R
+++ temp_timeseries/temp_time_series.R
@@ -27,52 +27,7 @@ dat <- read.csv("temperatureTimeSeries.csv")
 length(unique(dat$unique_ID)) # Good - 36 loggers
 
 
-
-# First we plot the temperature time series:
-
-# Aggregate to get mean per day for each logger. Otherwise the plot gets to messy.
-dat2 <- aggregate(data = dat,
-                  Temp~unique_ID+Date,
-                  FUN = mean)
-
-# then we get mean per date, and also the SD
-dat3 <- aggregate(data = dat2,
-                  Temp~Date,
-                  FUN = function(x) c(mn = mean(x), sd = sd(x)))
-
-head(dat3)
-str(dat3)
-# Obs, the output from the function doesn't show as columns
-
-dat4 <- do.call(data.frame, dat3)
-str(dat4)
-# Good
-
-
-
-# housekeeping - formatting the date
-dat4$Date2 <- as.POSIXct(dat4$Date, format = c("%Y-%m-%d"))
-
-
-
-# Plot temperature against date and add 1SD
-(temp.series <-  ggplot(data = dat4, 
-              aes(x = Date2, 
-                  y = Temp.mn)) +
-  geom_ribbon(aes(x = Date2, 
-                    ymax = Temp.mn+Temp.sd, 
-                    ymin = Temp.mn-Temp.sd, alpha = 0.2)) +
-  geom_line(size = 1) +
-  xlab("Date") +
-  ylab(expression(atop(paste("Temperature "), ( degree~C)))) +
-  theme_classic() +
-  theme(text = element_text(size = 15)) +
-  theme(legend.position = "none")
-  )
-
-
-
-# NEXT: Create a plot of daily temperature fluctuations.
+# Create a plot of daily temperature fluctuations.
 daily <- dat
 
 # first we get the max and min values per day and per logger
@@ -110,12 +65,9 @@ head(daily3)
 
 
 
-# Finally, plot the two figures together and export
-#grid.arrange(myPlot, myPlot2, nrow = 2)
-ggsave(filename = "Temperature time series and daily fluctuations.pdf",
-       plot = grid.arrange(temp.series, temp.fluct, nrow = 2),
-             height = 10, width = 10)
+# Save figure
+ggsave(filename = "Temperature daily fluctuations.pdf",
+       plot = temp.fluct, height = 5, width = 5)
 
-ggsave(filename = "temp_timeseries.png",
-       plot = grid.arrange(temp.series, temp.fluct, nrow = 2),
-       height = 10, width = 10)
+ggsave(filename = "temp_fluctuations.png",
+       plot = temp.fluct, height = 5, width = 5)
